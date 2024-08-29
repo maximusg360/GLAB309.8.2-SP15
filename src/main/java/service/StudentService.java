c528324c96ba8e4980ba9360905caf8bfbb7d5c7
Guzman <francisco.guzman@accenture.com>
2024-08-29 16:09:53 -0400

    lab done

---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,9 @@
+package service;
+
+import java.util.List;
+
+public interface StudentService {
+    void saveStudent(StudentDTO studentDto);
+    Student findStudentByEmail(String email);
+    List<StudentDTO> findAllStudents();
+}
---
+++ src/test/java/com/StudentLogin/StudentLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
